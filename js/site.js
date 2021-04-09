cbf5dcb4997e90a87cfeed502985ad6515f9f979
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-04-09 05:19:42 +0000

    add sweet alrert

--- js/site.js
+++ js/site.js
@@ -9,27 +9,32 @@ document.getElementById('form')
    const serviceID = 'default_service';
    const templateID = 'service_3x84eh8';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
 
-      Swal.fire({
-        title: 'Email Sent',
-        text: 'Your email was sent successfully. I will respond within 24 hours',
-        icon: 'success',
-        confirmButtonText: 'Great!'
-    })
-}, (err) => {
-    btn.value = 'Send Email';
-    Swal.fire({
-        title: 'Error!',
-        html: 'Looks like something went wrong! <hr /> I\'m always available on <a href="https://spencerleemeredith.com" target="_blank">LinkedInlink</a>',
-        icon: 'error',
-        confirmButtonText: 'Try Again'
-    })
+   emailjs.sendForm(serviceID, templateID, this)
+   .then(() => {
+       btn.value = 'Send Email';
+       swal({
+           title: "Message Sent!",
+           text: "I will respond within 24-48 hours!",
+           icon: "success",
+           button: "Back To Site!",
+       })
+   }, (err) => {
+       btn.value = 'Send Email';
+       swal({
+           title: "Error!",
+           text: "Ooops, looks like something went wrong!",
+           icon: "error",
+           button: "Try Again!",
+       });
+       alert(JSON.stringify(err));
+
+
+
+
+
+
+
 
 
 
