232ce743cde022782419daf8b719011af150d7d6
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-04-09 05:11:35 +0000

    add sweet alert add notficaiton alert add email js

--- js/site.js
+++ js/site.js
@@ -8,13 +8,30 @@ document.getElementById('form')
 
    const serviceID = 'default_service';
    const templateID = 'service_3x84eh8';
-   
+
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
       alert('Sent!');
     }, (err) => {
       btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+
+      Swal.fire({
+        title: 'Email Sent',
+        text: 'Your email was sent successfully. I will respond within 24 hours',
+        icon: 'success',
+        confirmButtonText: 'Great!'
+    })
+}, (err) => {
+    btn.value = 'Send Email';
+    Swal.fire({
+        title: 'Error!',
+        html: 'Looks like something went wrong! <hr /> I\'m always available on <a href="https://spencerleemeredith.com" target="_blank">LinkedInlink</a>',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+    })
+
+
+
     });
 });
